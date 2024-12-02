864b9862fa32295521cfe0b01cfbb97f3e266353
Sean Luo <seanluo@Seans-Small-MacBook-Pro.local>
2024-12-01 20:41:16 -0800

    add forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms'
 
 const config: Config = {
   content: [
@@ -17,6 +18,6 @@ const config: Config = {
       // },
     },
   },
-  plugins: [],
+  plugins: [forms],
 }
 export default config
